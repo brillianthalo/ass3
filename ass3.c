e7da6bb25915fab844c4eb0478c5e56b45a1f17e
Kilian Domes <kilian.domes@gmx.de>
2018-12-14 12:31:25 +0100

    initial commit

--- ass3.c
+++ ass3.c
@@ -1,3 +1,24 @@
+//TODO:
+//  - config einlesen und überprüfen
+//  - Fehlerverwaltung
+//      - out of memory
+//      - invalid file
+//      - invalid command
+//  - Spielfeld - Generierung
+//  - Initaler Spielstart
+//  - Usereingaben einlesen und verarbeiten
+//  - Spielmechaniken implementieren
+//  - help implementieren
+//  - exit UND EOF implementieren
+//  - win- und lose-condition implementieren
+//
+
+
+
+
+
+
+
 #include <stdio.h>
 
 int main() {
