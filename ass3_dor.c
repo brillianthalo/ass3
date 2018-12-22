eacc801ae6f255a79cbf38591898dfb3e2eead4a
brillianthalo <kilian.domes@gmx.de>
2018-12-22 21:18:08 +0100

    usereingaben funktionieren sind aber noch nicht richtig validiert

--- ass3_dor.c
+++ ass3_dor.c
@@ -24,27 +24,21 @@
 
 #define MAXSTACK 7
 #define DRAW_STACK 0
-#define GAME_STACK_1 1
-#define GAME_STACK_2 2
-#define GAME_STACK_3 3
-#define GAME_STACK_4 4
-#define DEP_STACK_1 5
-#define DEP_STACK_2 6
 #define MATCHFIELDHEIGHT 16
 typedef struct _Card_
 {
-  char color;
-  int value;
-  struct _Stack_ *stack;
-  struct _Card_ *next;
-  struct _Card_ *previous;
+  char color_;
+  int value_;
+  struct _Stack_ *stack_;
+  struct _Card_ *next_;
+  struct _Card_ *previous_;
 
 } Card;
 
 typedef struct _Stack_ {
-  Card *top_card;
-  char *stack_type;
-  Card *bottom_card;
+  Card *top_card_;
+  char *stack_type_;
+  Card *bottom_card_;
 } Stack;
 typedef enum _ReturnState_
 {
@@ -52,200 +46,10 @@ typedef enum _ReturnState_
   INVALID_ARGS = 1,
   OUT_OF_MEMORY = 2,
   INVALID_CONFIG_FILE = 3,
-} ReturnState;
-typedef enum _InfoState_ {
-  VALID_COMMAND = 0,
   INVALID_COMMAND = -1,
   INVALID_MOVE = -2
-} InfoState;
+} ReturnState;
 
-
-//forward function declaration
-ReturnState readInitFile(const char *path, Stack *draw_stack);
-int movePile(Stack *moving_pile, Stack *to_stack);
-int initialHandOut(Stack *stack_array);
-InfoState checkValidMove(Stack *moving_pile, Stack *destination_stack);
-ReturnState printErrorMessage(ReturnState return_value);
-//int printStack(Stack *stack);
-int printMatchfield(Stack *stack_array);
-int printHelp();
-Stack* findCardPileByColorValue(char color, int value, Stack *stack_array);
-InfoState printInfoMessage(InfoState info_message);
-int freeAllCardMems(Stack *stack_array);
-int exitGame(Stack *stack_array);
-int getValueAsInt(char current_character);
-char* ignoreBlankspaces(char *string);
-
-int main(int argc, char* argv[]) {
-  int exit_status = 1;
-
-  char input_color;
-  int input_value;
-  int input_stack;
-  Stack stack_array[7] = {{NULL, "DRAW", NULL},
-                          {NULL, "GAME", NULL},
-                          {NULL, "GAME", NULL},
-                          {NULL, "GAME", NULL},
-                          {NULL, "GAME", NULL},
-                          {NULL, "DEPOSIT", NULL},
-                          {NULL, "DEPOSIT", NULL}};
-
-  ReturnState return_value = readInitFile(argv[1], &(stack_array[DRAW_STACK]));
-
-  if(return_value != OK)
-  {
-    return printErrorMessage(return_value);
-  }
-
-  initialHandOut(stack_array);
-  printMatchfield(stack_array);
-  while(exit_status)
-  {
-
-    printf("esp> ");
-
-    char *input_memory_location = calloc(1, 1);
-
-    unsigned int current_input_size = 0;
-    if(input_memory_location != NULL)
-    {
-      char current_input_char;
-      while((current_input_char = getchar()) != '\n' && current_input_char != EOF)
-      {
-        input_memory_location[current_input_size] = toupper(current_input_char);
-        input_memory_location = realloc(input_memory_location, ++current_input_size);
-      }
-      if(current_input_char == EOF)
-      {
-
-        return 0;
-      }
-      input_memory_location[current_input_size] = '\0';
-    }
-    else
-    {
-      return OUT_OF_MEMORY;
-    }
-    if(current_input_size == 1 )
-    {
-      free(input_memory_location);
-      continue;
-    }
-    char *input = input_memory_location;
-
-    input = ignoreBlankspaces(input);
-    if(strncmp(input, "HELP", 4) == 0)
-    {
-      printHelp();
-    }
-    else if(strncmp(input, "MOVE", 4) == 0)
-    {
-      input += 4;
-      input = ignoreBlankspaces(input);
-      if(strncmp(input, "RED", 3) == 0)
-      {
-        input_color = 'R';
-        input += 3;
-      }
-      else if(strncmp(input, "BLACK", 5) == 0)
-      {
-        input_color = 'B';
-        input += 5;
-      }
-      else
-      {
-        free(input_memory_location);
-        printInfoMessage(INVALID_COMMAND);
-        continue;
-      }
-      input = ignoreBlankspaces(input);
-      if(input[0] == '1' && input[1] == '0')
-      {
-        input_value = 10;
-        input += 2;
-      }
-      else if((input_value = getValueAsInt(*input)) > 0)
-      {
-        input += 1;
-      } 
-      else
-      {
-        free(input_memory_location);
-        printInfoMessage(INVALID_COMMAND);
-        continue;
-      }
-      input = ignoreBlankspaces(input);
-      if(strncmp(input, "TO", 2) == 0)
-      {
-        input += 2;
-      }
-      else
-      {
-        free(input_memory_location);
-        printInfoMessage(INVALID_COMMAND);
-        continue;
-      }
-      input = ignoreBlankspaces(input);
-      if(input[0] > '0' && input[0] < '7')
-      {
-        input_stack = input[0] - '0';
-        input++;
-      }
-      else
-      {
-        free(input_memory_location);
-        printInfoMessage(INVALID_COMMAND);
-        continue;
-      }
-      input = ignoreBlankspaces(input);
-      if (input[0] != '\0')
-      {
-        free(input_memory_location);
-        printInfoMessage(INVALID_COMMAND);
-        continue;
-      }
-      Stack *pile_to_move = findCardPileByColorValue(input_color, input_value, stack_array);
-      if(pile_to_move == NULL)
-      {
-        printErrorMessage(OUT_OF_MEMORY);
-        free(input_memory_location);
-        return OUT_OF_MEMORY;
-      }
-      else if (pile_to_move->bottom_card == NULL)
-      {
-        printInfoMessage(INVALID_MOVE);
-        free(input_memory_location);
-        continue;
-      }
-      InfoState info_state = checkValidMove(pile_to_move, &stack_array[input_stack]);
-      if(info_state == VALID_COMMAND)
-      {
-        movePile(pile_to_move, &stack_array[input_stack]);
-        free(input_memory_location);
-        printMatchfield(stack_array);
-      }
-      else
-      {
-        printInfoMessage(INVALID_MOVE);
-        free(input_memory_location)
-        continue;
-      }
-    }
-    else if(strncmp(input, "EXIT", 4) == 0 || input[0] == EOF)
-    {
-      printf("EXIT or EOF\n");
-      free(input_memory_location);
-      freeAllCardMems(stack_array);
-      exit_status = 0;
-    }
-    else
-    {
-        printInfoMessage(INVALID_COMMAND);
-        free(input_memory_location);
-    }  
-  }
-  return 0;
-}
 
 char* ignoreBlankspaces(char *string)
 {
@@ -260,12 +64,12 @@ int freeAllCardMems(Stack *stack_array)
 {
   for(int current_stack = 0; current_stack < MAXSTACK; current_stack++)
   {
-    Card *head_card = (stack_array + current_stack)->top_card;
+    Card *head_card = (stack_array + current_stack)->top_card_;
     Card *tmp_card;
     while(head_card != NULL)
     {
       tmp_card = head_card;
-      head_card = head_card->next;
+      head_card = head_card->next_;
       free(tmp_card);
     }
   }
@@ -291,6 +95,12 @@ ReturnState printErrorMessage(ReturnState return_value)
     case OUT_OF_MEMORY:
       printf("[ERR] Out of memory.\n");
       break;
+    case INVALID_COMMAND:
+      printf("[INFO] Invalid command!\n");
+      break;
+    case INVALID_MOVE:
+      printf("[INFO] Invalid move command!\n");
+      break;
     case OK:
       //left blank because nothing should happen
       break;
@@ -298,85 +108,69 @@ ReturnState printErrorMessage(ReturnState return_value)
   return return_value;
 }
 
-InfoState printInfoMessage(InfoState info_message)
-{
-  switch(info_message)
-  {
-    case INVALID_COMMAND:
-      printf("[INFO] Invalid command!\n");
-      break;
-    case INVALID_MOVE:
-      printf("[INFO] Invalid move command!\n");
-      break;
-    case VALID_COMMAND:
-      //left blank because nothing should happen
-      break;
-  }
-  return info_message;
-}
 
 Stack* makeSingleCardToPile(Card *card)
 {
   Stack *pile = malloc(sizeof(Stack));
-  pile->bottom_card = card;
-  pile->top_card = card;
+  pile->bottom_card_ = card;
+  pile->top_card_ = card;
   return pile;
 }
 
 
 int addPileToStackTop(Stack *add_pile, Stack *card_stack)
 {
-  Card *new_card = add_pile->bottom_card;
-  new_card->next = card_stack->top_card;
-  if(card_stack->top_card != NULL)
+  Card *new_card = add_pile->bottom_card_;
+  new_card->next_ = card_stack->top_card_;
+  if(card_stack->top_card_ != NULL)
   {
-    card_stack->top_card->previous = new_card;
+    card_stack->top_card_->previous_ = new_card;
   }
-  card_stack->top_card = add_pile->top_card;
-  if(card_stack->bottom_card == NULL)
+  card_stack->top_card_ = add_pile->top_card_;
+  if(card_stack->bottom_card_ == NULL)
   {
-    card_stack->bottom_card = new_card;
+    card_stack->bottom_card_ = new_card;
   }
   while(new_card != NULL)
   {
-    new_card->stack = card_stack;
-    new_card = new_card->previous;
+    new_card->stack_ = card_stack;
+    new_card = new_card->previous_;
   }
   free(add_pile);
   return 0;
 }
 
-InfoState checkValidMove(Stack *moving_pile, Stack *destination_stack)
+ReturnState checkValidMove(Stack *moving_pile, Stack *destination_stack)
 {
-  Card *moving_card = moving_pile->bottom_card;
-  char *stack_type = destination_stack->stack_type;
-  Card *destination_card = destination_stack->top_card;
+  Card *moving_card = moving_pile->bottom_card_;
+  char *stack_type = destination_stack->stack_type_;
+  Card *destination_card = destination_stack->top_card_;
 
-  if (strcmp(destination_stack->stack_type, "DRAW") == 0)
+  if (strcmp(destination_stack->stack_type_, "DRAW") == 0)
   {
     free(moving_pile);
     return INVALID_MOVE;
   }
   else if (destination_card == NULL) {
-    if (strcmp(destination_stack->stack_type, "DEPOSIT") == 0 && moving_card->value != 1) {
+    if (strcmp(destination_stack->stack_type_, "DEPOSIT") == 0 && moving_card->value_ != 1) {
       free(moving_pile);
       return INVALID_MOVE;
     }
-    else if (strcmp(destination_stack->stack_type, "GAME") == 0
-      && moving_card->value != 13)
+    else if (strcmp(destination_stack->stack_type_, "GAME") == 0
+      && moving_card->value_ != 13)
     {
       free(moving_pile);
       return INVALID_MOVE;
     }
     else
     {
-      return VALID_COMMAND;
+      return OK;
     }
   }
   else if (strcmp(stack_type, "DEPOSIT") == 0)
   {
-    if(moving_card->value != (destination_card->value + 1)
-       || moving_card->color != destination_card->color)
+    if(moving_card->value_ != (destination_card->value_ + 1)
+       || moving_card->color_ != destination_card->color_)
     {
       free(moving_pile);
       return INVALID_MOVE;
@@ -384,14 +178,14 @@ InfoState checkValidMove(Stack *moving_pile, Stack *destination_stack)
   }
   else
   {
-    if (moving_card->value != (destination_card->value - 1)
-      || moving_card->color == destination_card->color)
+    if (moving_card->value_ != (destination_card->value_ - 1)
+      || moving_card->color_ == destination_card->color_)
     {
       free(moving_pile);
       return INVALID_MOVE;
     }
   }
-  return VALID_COMMAND;
+  return OK;
 }
 
 Stack* findCardPileByColorValue (char color, int value, Stack *stack_array)
@@ -402,12 +196,12 @@ Stack* findCardPileByColorValue (char color, int value, Stack *stack_array)
   {
     return moving_pile;
   }
-  if((stack_array + DRAW_STACK)->top_card != NULL)
+  if((stack_array + DRAW_STACK)->top_card_ != NULL)
   {
-    moving_pile->top_card = (stack_array + DRAW_STACK)->top_card;
-    moving_pile->bottom_card = moving_pile->top_card;
-    current_card = moving_pile->bottom_card;
-    if (current_card->color == color && current_card->value == value)
+    moving_pile->top_card_ = (stack_array + DRAW_STACK)->top_card_;
+    moving_pile->bottom_card_ = moving_pile->top_card_;
+    current_card = moving_pile->bottom_card_;
+    if (current_card->color_ == color && current_card->value_ == value)
     {
       return moving_pile;
     }
@@ -415,40 +209,40 @@ Stack* findCardPileByColorValue (char color, int value, Stack *stack_array)
   for(int current_stack_number = 1; current_stack_number < (MAXSTACK - 2);
     current_stack_number++)
   {
-    moving_pile->top_card = (stack_array + current_stack_number)->top_card;
-    current_card = (stack_array + current_stack_number)->top_card;
+    moving_pile->top_card_ = (stack_array + current_stack_number)->top_card_;
+    current_card = (stack_array + current_stack_number)->top_card_;
     while(current_card != NULL)
     {
-      if(current_card->color == color && current_card->value == value)
+      if(current_card->color_ == color && current_card->value_ == value)
       {
-        moving_pile->bottom_card = current_card;
+        moving_pile->bottom_card_ = current_card;
         return moving_pile;
       }
-      current_card = current_card->next;
+      current_card = current_card->next_;
       if(current_card != NULL &&
-        (current_card->color == current_card->previous->color ||
-        current_card->value != current_card->previous->value + 1))
+        (current_card->color_ == current_card->previous_->color_ ||
+        current_card->value_ != current_card->previous_->value_ + 1))
       {
         break;
       }
     }
   }
-  moving_pile->top_card = NULL;
-  moving_pile->bottom_card = NULL;
+  moving_pile->top_card_ = NULL;
+  moving_pile->bottom_card_ = NULL;
   return moving_pile;
 }
 
 int movePile(Stack *moving_pile, Stack *to_stack)
 {
-  Card* moving_card = moving_pile->bottom_card;
-  moving_card->stack->top_card = moving_card->next;
-  if(moving_card->next != NULL)
+  Card* moving_card = moving_pile->bottom_card_;
+  moving_card->stack_->top_card_ = moving_card->next_;
+  if(moving_card->next_ != NULL)
   {
-    moving_card->next->previous = NULL;
+    moving_card->next_->previous_ = NULL;
   }
   else
   {
-    moving_card->stack->bottom_card = NULL;
+    moving_card->stack_->bottom_card_ = NULL;
   }
   addPileToStackTop(moving_pile, to_stack);
   return 0;
@@ -461,7 +255,7 @@ int initialHandOut (Stack *stack_array)
     for(int distribute = (1 + offset); distribute <= 4; distribute++)
     {
      // printf("distribute: %i\n", distribute);
-      Stack *move_pile = makeSingleCardToPile((stack_array + DRAW_STACK)->top_card);
+      Stack *move_pile = makeSingleCardToPile((stack_array + DRAW_STACK)->top_card_);
       movePile(move_pile, &stack_array[distribute]);
     }
   }
@@ -541,11 +335,11 @@ Card* createNewCard(char color, int value, Stack *stack)
   {
     return NULL;
   }
-  new_card->color = color;
-  new_card->value = value;
-  new_card->stack = stack;
-  new_card->next = NULL;
-  new_card->previous = NULL;
+  new_card->color_ = color;
+  new_card->value_ = value;
+  new_card->stack_ = stack;
+  new_card->next_ = NULL;
+  new_card->previous_ = NULL;
   return new_card;
 }
 
@@ -704,7 +498,7 @@ int printMatchfield (Stack *stack_array)
   Card* current_pointer[MAXSTACK];
   for(int pointer_copy = 0; pointer_copy < MAXSTACK; pointer_copy++)
   {
-    *(current_pointer + pointer_copy) = (stack_array + pointer_copy)->bottom_card;
+    *(current_pointer + pointer_copy) = (stack_array + pointer_copy)->bottom_card_;
   }
   for(int current_matchfield_row = 0; current_matchfield_row < MATCHFIELDHEIGHT;
     current_matchfield_row++)
@@ -723,23 +517,197 @@ int printMatchfield (Stack *stack_array)
         current_color = ' ';
         strcpy(current_value_to_s, "  ");
       }
-      else if (current_col == DRAW_STACK && current_card->previous != NULL)
+      else if (current_col == DRAW_STACK && current_card->previous_ != NULL)
       {
         current_color = 'X';
         strcpy(current_value_to_s, "  ");
       }
       else
       {
-        current_color = current_card->color;
-        current_value = current_card->value;
+        current_color = current_card->color_;
+        current_value = current_card->value_;
         getValueAsString(current_value, current_value_to_s);
       }
       printf("%c%s%s", current_color, current_value_to_s, current_col_end);
       current_pointer[current_col] = (current_card != NULL)
-        ? current_card->previous : NULL;
+        ? current_card->previous_ : NULL;
 
     }
     printf("\n");
   }
+  return 0;
+}
+
+ReturnState doMove(char *input, Stack *stack_array)
+{
+  char input_color;
+  int input_value;
+  int input_stack;
+  ReturnState return_value;
+  input = ignoreBlankspaces(input);
+  if(strncmp(input, "RED", 3) == 0)
+  {
+    input_color = 'R';
+    input += 3;
+  }
+  else if(strncmp(input, "BLACK", 5) == 0)
+  {
+    input_color = 'B';
+    input += 5;
+  }
+  else
+  {
+    return INVALID_COMMAND;
+  }
+  input = ignoreBlankspaces(input);
+  if(input[0] == '1' && input[1] == '0')
+  {
+    input_value = 10;
+    input += 2;
+  }
+  else if((input_value = getValueAsInt(input[0])) > 0)
+  {
+    input += 1;
+  }
+  else
+  {
+    return INVALID_COMMAND;
+  }
+  input = ignoreBlankspaces(input);
+  if(strncmp(input, "TO", 2) == 0)
+  {
+    input += 2;
+  }
+  else
+  {
+    return INVALID_COMMAND;
+  }
+  input = ignoreBlankspaces(input);
+  if(input[0] > '0' && input[0] < '7')
+  {
+    input_stack = input[0] - '0';
+    input++;
+  }
+  else
+  {
+    return INVALID_COMMAND;
+  }
+  input = ignoreBlankspaces(input);
+
+  Stack *pile_to_move = findCardPileByColorValue(input_color, input_value, stack_array);
+  if(pile_to_move == NULL)
+  {
+    return OUT_OF_MEMORY;
+  }
+  else if (pile_to_move->bottom_card_ == NULL)
+  {
+    free(pile_to_move);
+    return INVALID_MOVE;
+  }
+  else
+  {
+    return_value = checkValidMove(pile_to_move, &stack_array[input_stack]);
+    movePile(pile_to_move, &stack_array[input_stack]);
+    return return_value;
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  int exit_status = 1;
+
+  Stack stack_array[7] = {{NULL, "DRAW", NULL},
+                          {NULL, "GAME", NULL},
+                          {NULL, "GAME", NULL},
+                          {NULL, "GAME", NULL},
+                          {NULL, "GAME", NULL},
+                          {NULL, "DEPOSIT", NULL},
+                          {NULL, "DEPOSIT", NULL}};
+
+  ReturnState return_value = readInitFile(argv[1], &(stack_array[DRAW_STACK]));
+
+  if(return_value != OK)
+  {
+    return printErrorMessage(return_value);
+  }
+
+  initialHandOut(stack_array);
+  printMatchfield(stack_array);
+  while(exit_status)
+  {
+
+    printf("esp> ");
+
+    char *input_memory_location = malloc(1);
+
+    unsigned int current_input_size = 0;
+    if(input_memory_location != NULL)
+    {
+      input_memory_location[0] = '\n';
+      char current_input_char;
+      while((current_input_char = getchar()) != '\n' && current_input_char != '\r' && current_input_char != EOF)
+      {
+        input_memory_location[current_input_size] = toupper(current_input_char);
+        input_memory_location = realloc(input_memory_location, ++current_input_size);
+      }
+      if(current_input_char == EOF)
+      {
+        freeAllCardMems(stack_array);
+        free(input_memory_location);
+        return 0;
+      }
+      input_memory_location[current_input_size] = '\0';
+    }
+    else
+    {
+      freeAllCardMems(stack_array);
+      return OUT_OF_MEMORY;
+    }
+    if(current_input_size == 0)
+    {
+      free(input_memory_location);
+      continue;
+    }
+    char *input = input_memory_location;
+
+    input = ignoreBlankspaces(input);
+    if(strncmp(input, "HELP", 4) == 0)
+    {
+      printHelp();
+    }
+    else if(strncmp(input, "MOVE", 4) == 0)
+    {
+      input += 4;
+      ReturnState move_return_state = doMove(input, stack_array);
+      if(move_return_state == OK)
+      {
+        free(input_memory_location);
+        printMatchfield(stack_array);
+      }
+      else if(move_return_state < 0)
+      {
+        printErrorMessage(move_return_state);
+      }
+      else
+      {
+        freeAllCardMems(stack_array);
+        free(input_memory_location);
+        return move_return_state;
+      }
+    }
+    else if(strncmp(input, "EXIT", 4) == 0 || input[0] == EOF)
+    {
+      printf("EXIT or EOF\n");
+      free(input_memory_location);
+      freeAllCardMems(stack_array);
+      exit_status = 0;
+    }
+    else
+    {
+      printErrorMessage(INVALID_COMMAND);
+      free(input_memory_location);
+    }
+  }
+  freeAllCardMems(stack_array);
   return 0;
 }
